83af20d86747e72aaab1cc08aa1da62627947386
Abhishek-Rana-weboapp <abhishek@weboappdiscovery.com>
2024-10-06 14:00:34 +0530

    Provide Page Done

---
+++ src/components/AnimatedHeading.tsx
@@ -0,0 +1,25 @@
+import { easeInOut, motion } from "framer-motion";
+
+type Props = {
+    children: React.ReactNode;
+}
+
+export const AnimatedHeading = ({children} : Props) => {
+  return (
+    <motion.h1
+      variants={{
+        initial: { opacity: 0, y: 20 },
+        animate: { opacity: 1, y: 0 },
+      }}
+      initial="initial"
+      whileInView="animate"
+      transition={{ duration: 0.6, ease: easeInOut }}
+      viewport={{ once: true, amount: 0.5 }}
+      className="text-center md:text-5xl text-2xl z-20 font-medium mx-auto  px-5 corner-border"
+    >
+      <span className="relative z-10 px-4 py-4 w-full">
+        {children}
+      </span>
+    </motion.h1>
+  );
+};
---
+++ src/data/courses.ts
@@ -0,0 +1,22 @@
+export const courses = [
+    {
+        title: "Digital Marketing",
+        description: "Learn the fundamentals of digital marketing, including SEO, social media, email marketing",
+        img: "/DigitalMarketing.jpg"
+    },
+    {
+        title: "Data Science",
+        description: "Learn the fundamentals of data science, including data visualization, machine learning, and statistical",
+        img: "/DataScience.jpg"
+    },
+    {
+        title: "Artificial Intelligence",
+        description: "Learn the fundamentals of artificial intelligence, including natural language processing, computer vision, and robotics",
+        img: "/AI.jpg"
+    },
+    {
+        title: "Cyber Security",
+        description: "Learn the fundamentals of cyber security, including threat analysis, risk management, and incident",
+        img: "/DigitalMarketing.jpg"
+    }
+]
