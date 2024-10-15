37cbed0399d013dd88a8418565282b9a622ebfde
Abhishek-Rana-weboapp <abhishek@weboappdiscovery.com>
2024-10-15 22:49:38 +0530

    fixes

--- src/pages/authentication/Login.tsx
+++ src/pages/authentication/Login.tsx
@@ -27,7 +27,7 @@ export const Login = () => {
   });
   const [showPassword, setShowPassword] = useState(false);
 
-  const { user, setUser } = useAuth();
+  const {setUser } = useAuth();
 
   const handleLogin: SubmitHandler<FormFields> = async (data) => {
     if (data) {
